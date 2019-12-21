03bd9e6db8e7f21103db9c817917badf3525e1e1
josharsh <harsh.joshi.pth@gmail.com>
2019-12-21 16:25:36 +0530

    Table of Contents Updated

--- app/src/main/java/com/example/androidlabs/TableOfContents.java
+++ app/src/main/java/com/example/androidlabs/TableOfContents.java
@@ -11,12 +11,57 @@ import com.example.androidlabs.Experiment10.Experiment10;
 import com.example.androidlabs.Experiment9.Experiment9;
 
 public class TableOfContents extends AppCompatActivity {
-Button exp6,exp7,exp8,exp9,exp10;
+Button exp1,exp2,exp3,exp4,exp5,exp6,exp7,exp8,exp9,exp10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table_of_contents);
-        exp6=(Button)findViewById(R.id.button5);
+        exp1=(Button)findViewById(R.id.exp1Btn);
+        exp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(TableOfContents.this,Exp6.class);
+                startActivity(i);
+
+            }
+        });
+        exp2=(Button)findViewById(R.id.exp2Btn);
+        exp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(TableOfContents.this,Exp6.class);
+                startActivity(i);
+
+            }
+        });
+        exp3=(Button)findViewById(R.id.exp3Btn);
+        exp3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(TableOfContents.this,Exp6.class);
+                startActivity(i);
+
+            }
+        });
+        exp4=(Button)findViewById(R.id.exp4Btn);
+        exp4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(TableOfContents.this,Exp6.class);
+                startActivity(i);
+
+            }
+        });
+        exp5=(Button)findViewById(R.id.exp5Btn);
+        exp5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(TableOfContents.this,Exp6.class);
+                startActivity(i);
+
+            }
+        });
+        exp6=(Button)findViewById(R.id.exp6Btn);
         exp6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
